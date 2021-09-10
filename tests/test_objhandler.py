931e002de2a282fd45f64c4bfcd92c88cca08e63
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-09-10 14:48:24 +0200

    convert to pytest

--- tests/test_objhandler.py
+++ tests/test_objhandler.py
@@ -10,172 +10,171 @@ from smithers.io.obj.objparser import save_obj, WavefrontOBJ
 data = ObjHandler.read("tests/test_datasets/file.obj")
 
 
-class TestObjHandler(TestCase):
-    def test_vertices(self):
-        expected = [
+def test_vertices():
+    expected = [
+        [0.109625, 0.06, -0.0488084],
+        [0.0488084, 0.06, -0.109625],
+        [0.0488084, 0.06, -0.2],
+    ]
+
+    np.testing.assert_almost_equal(data.vertices, expected, decimal=7)
+
+def test_polygons():
+    expected = [[0, 1, 2], [2, 1, 0]]
+
+    np.testing.assert_almost_equal(data.polygons, expected, decimal=7)
+
+def test_regions():
+    assert data.regions == ["inner", "outer"]
+    assert data.regions_change_indexes[0] == (0, 0)
+    assert data.regions_change_indexes[1] == (1, 1)
+
+def test_write():
+    x = WavefrontOBJ()
+    x.regions = ["inner", "outer"]
+    x.regions_change_indexes = [(0, 0), (1, 1)]
+    x.vertices = [
+        [0.109625, 0.06, -0.0488084],
+        [0.0488084, 0.06, -0.109625],
+        [0.0488084, 0.06, -0.2],
+    ]
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
+
+    save_obj(x, "/var/tmp/data.obj")
+    assert cmp("/var/tmp/data.obj", "tests/test_datasets/file.obj")
+
+def test_boundary():
+    x = WavefrontOBJ()
+    x.vertices = np.array(
+        [
             [0.109625, 0.06, -0.0488084],
             [0.0488084, 0.06, -0.109625],
             [0.0488084, 0.06, -0.2],
         ]
+    )
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
 
-        np.testing.assert_almost_equal(data.vertices, expected, decimal=7)
+    exp = np.array([[0.0488084, 0.06, -0.2], [0.109625, 0.06, -0.0488084]])
 
-    def test_polygons(self):
-        expected = [[0, 1, 2], [2, 1, 0]]
+    np.testing.assert_almost_equal(
+        ObjHandler.boundary(x, axis=None), exp, decimal=15
+    )
 
-        np.testing.assert_almost_equal(data.polygons, expected, decimal=7)
+    np.testing.assert_almost_equal(
+        ObjHandler.boundary(x, axis=2), exp[:,2], decimal=15
+    )
 
-    def test_regions(self):
-        assert data.regions == ["inner", "outer"]
-        assert data.regions_change_indexes[0] == (0, 0)
-        assert data.regions_change_indexes[1] == (1, 1)
-
-    def test_write(self):
-        x = WavefrontOBJ()
-        x.regions = ["inner", "outer"]
-        x.regions_change_indexes = [(0, 0), (1, 1)]
-        x.vertices = [
+def test_translate():
+    x = WavefrontOBJ()
+    x.vertices = np.array(
+        [
             [0.109625, 0.06, -0.0488084],
             [0.0488084, 0.06, -0.109625],
             [0.0488084, 0.06, -0.2],
         ]
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
+    )
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
 
-        save_obj(x, "/var/tmp/data.obj")
-        assert cmp("/var/tmp/data.obj", "tests/test_datasets/file.obj")
+    ObjHandler.translate(x, [1, 0, -1])
 
-    def test_boundary(self):
-        x = WavefrontOBJ()
-        x.vertices = np.array(
+    assert x.polygons == [[0, 1, 2], [2, 1, 0]]
+
+    expected = np.array(
+        [
+            [1.109625, 0.06, -1.0488084],
+            [1.0488084, 0.06, -1.109625],
+            [1.0488084, 0.06, -1.2],
+        ]
+    )
+    np.testing.assert_almost_equal(x.vertices, expected, decimal=15)
+
+def test_rotate():
+    x = WavefrontOBJ()
+    x.vertices = np.array(
+        [
+            [0.109625, 0.06, -0.0488084],
+            [0.0488084, 0.06, -0.109625],
+            [0.0488084, 0.06, -0.2],
+        ]
+    )
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
+
+    ObjHandler.rotate_around_axis(x, [0, 0, 1], np.pi)
+
+    assert x.polygons == [[0, 1, 2], [2, 1, 0]]
+
+    expected = np.array(
+        [
+            [-0.109625, -0.06, -0.0488084],
+            [-0.0488084, -0.06, -0.109625],
+            [-0.0488084, -0.06, -0.2],
+        ]
+    )
+    np.testing.assert_almost_equal(x.vertices, expected, decimal=15)
+
+def test_scale():
+    x = WavefrontOBJ()
+    tmp = np.array(
+        [
+            [0.109625, 0.06, -0.0488084],
+            [0.0488084, 0.06, -0.109625],
+            [0.0488084, 0.06, -0.2],
+        ]
+    )
+    x.vertices = np.array(tmp)
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
+
+    ObjHandler.scale(x, [0.01, 0.001, 1])
+
+    assert x.polygons == [[0, 1, 2], [2, 1, 0]]
+
+    expected = np.array(
+        [
+            [0.00109625, 0.00006, -0.0488084],
+            [0.000488084, 0.00006, -0.109625],
+            [0.000488084, 0.00006, -0.2],
+        ]
+    )
+    np.testing.assert_almost_equal(x.vertices, expected, decimal=15)
+
+def test_dimension():
+    x = WavefrontOBJ()
+    tmp = np.array(
+        [
+            [0.109625, 0.06, -0.0488084],
+            [0.0488084, 0.06, -0.109625],
+            [0.0488084, 0.06, -0.2],
+        ]
+    )
+    x.vertices = np.array(tmp)
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
+
+    np.testing.assert_almost_equal(
+        ObjHandler.dimension(x),
+        np.array(
             [
-                [0.109625, 0.06, -0.0488084],
-                [0.0488084, 0.06, -0.109625],
-                [0.0488084, 0.06, -0.2],
+                0.109625 - 0.0488084,
+                0,
+                -0.0488084 + 0.2,
             ]
-        )
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
+        ),
+        decimal=15,
+    )
 
-        exp = np.array([[0.0488084, 0.06, -0.2], [0.109625, 0.06, -0.0488084]])
+def test_switch_axes():
+    x = WavefrontOBJ()
+    tmp = np.array(
+        [
+            [0.109625, 0.06, -0.0488084],
+            [0.0488084, 0.06, -0.109625],
+            [0.0488084, 0.06, -0.2],
+        ]
+    )
+    x.vertices = np.array(tmp)
+    x.polygons = [[0, 1, 2], [2, 1, 0]]
 
-        np.testing.assert_almost_equal(
-            ObjHandler.boundary(x, axis=None), exp, decimal=15
-        )
+    ObjHandler.switch_axes(x, 1, 2)
 
-        np.testing.assert_almost_equal(
-            ObjHandler.boundary(x, axis=2), exp[:,2], decimal=15
-        )
-
-    def test_translate(self):
-        x = WavefrontOBJ()
-        x.vertices = np.array(
-            [
-                [0.109625, 0.06, -0.0488084],
-                [0.0488084, 0.06, -0.109625],
-                [0.0488084, 0.06, -0.2],
-            ]
-        )
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
-
-        ObjHandler.translate(x, [1, 0, -1])
-
-        assert x.polygons == [[0, 1, 2], [2, 1, 0]]
-
-        expected = np.array(
-            [
-                [1.109625, 0.06, -1.0488084],
-                [1.0488084, 0.06, -1.109625],
-                [1.0488084, 0.06, -1.2],
-            ]
-        )
-        np.testing.assert_almost_equal(x.vertices, expected, decimal=15)
-
-    def test_rotate(self):
-        x = WavefrontOBJ()
-        x.vertices = np.array(
-            [
-                [0.109625, 0.06, -0.0488084],
-                [0.0488084, 0.06, -0.109625],
-                [0.0488084, 0.06, -0.2],
-            ]
-        )
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
-
-        ObjHandler.rotate_around_axis(x, [0, 0, 1], np.pi)
-
-        assert x.polygons == [[0, 1, 2], [2, 1, 0]]
-
-        expected = np.array(
-            [
-                [-0.109625, -0.06, -0.0488084],
-                [-0.0488084, -0.06, -0.109625],
-                [-0.0488084, -0.06, -0.2],
-            ]
-        )
-        np.testing.assert_almost_equal(x.vertices, expected, decimal=15)
-
-    def test_scale(self):
-        x = WavefrontOBJ()
-        tmp = np.array(
-            [
-                [0.109625, 0.06, -0.0488084],
-                [0.0488084, 0.06, -0.109625],
-                [0.0488084, 0.06, -0.2],
-            ]
-        )
-        x.vertices = np.array(tmp)
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
-
-        ObjHandler.scale(x, [0.01, 0.001, 1])
-
-        assert x.polygons == [[0, 1, 2], [2, 1, 0]]
-
-        expected = np.array(
-            [
-                [0.00109625, 0.00006, -0.0488084],
-                [0.000488084, 0.00006, -0.109625],
-                [0.000488084, 0.00006, -0.2],
-            ]
-        )
-        np.testing.assert_almost_equal(x.vertices, expected, decimal=15)
-
-    def test_dimension(self):
-        x = WavefrontOBJ()
-        tmp = np.array(
-            [
-                [0.109625, 0.06, -0.0488084],
-                [0.0488084, 0.06, -0.109625],
-                [0.0488084, 0.06, -0.2],
-            ]
-        )
-        x.vertices = np.array(tmp)
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
-
-        np.testing.assert_almost_equal(
-            ObjHandler.dimension(x),
-            np.array(
-                [
-                    0.109625 - 0.0488084,
-                    0,
-                    -0.0488084 + 0.2,
-                ]
-            ),
-            decimal=15,
-        )
-
-    def test_switch_axes(self):
-        x = WavefrontOBJ()
-        tmp = np.array(
-            [
-                [0.109625, 0.06, -0.0488084],
-                [0.0488084, 0.06, -0.109625],
-                [0.0488084, 0.06, -0.2],
-            ]
-        )
-        x.vertices = np.array(tmp)
-        x.polygons = [[0, 1, 2], [2, 1, 0]]
-
-        ObjHandler.switch_axes(x, 1, 2)
-
-        np.testing.assert_almost_equal(
-            x.vertices, tmp[:, [0, 2, 1]], decimal=15
-        )
+    np.testing.assert_almost_equal(
+        x.vertices, tmp[:, [0, 2, 1]], decimal=15
+    )
--- tests/test_openfoamhandler.py
+++ tests/test_openfoamhandler.py
@@ -2,104 +2,162 @@ from unittest import TestCase
 import unittest
 import numpy as np
 import Ofpp
-from smithers.io.openfoamhandler import OpenFoamHandler
+from smithers.io.openfoam import OpenFoamHandler
 
-openfoam_mesh_path = 'tests/test_datasets/openfoam_mesh'
-notime_openfoam_mesh_path = 'tests/test_datasets/notime_openfoam_mesh'
-
+openfoam_mesh_path = "tests/test_datasets/openfoam_mesh"
+notime_openfoam_mesh_path = "tests/test_datasets/notime_openfoam_mesh"
 handler = OpenFoamHandler()
 mesh = handler.read(openfoam_mesh_path)
 truth_mesh = Ofpp.FoamMesh(openfoam_mesh_path)
 
-class TestOpenFoamHandler(TestCase):
-    def test_read(self):
-        assert type(mesh) == dict
 
-        assert 'points' in mesh['0']
-        assert 'faces' in mesh['0']
-        assert 'boundary' in mesh['0']
-        assert 'cells' in mesh['0']
+def test_read():
+    assert type(mesh) == dict
 
-    def test_read_boundary_names(self):
-        assert set(mesh['0']['boundary'].keys()) == set([b'inlet', b'outlet', b'bottom', b'top', b'obstacle', b'frontAndBack'])
+    assert "points" in mesh["0"]
+    assert "faces" in mesh["0"]
+    assert "boundary" in mesh["0"]
+    assert "cells" in mesh["0"]
 
-    def test_read_points(self):
-        np.testing.assert_almost_equal(mesh['0']['points'], truth_mesh.points)
 
-    def test_read_faces(self):
-        np.testing.assert_almost_equal(mesh['0']['faces'], truth_mesh.faces)
+def test_read_boundary_names():
+    assert set(mesh["0"]["boundary"].keys()) == set(
+        [
+            b"inlet",
+            b"outlet",
+            b"bottom",
+            b"top",
+            b"obstacle",
+            b"frontAndBack",
+        ]
+    )
 
-    def test_read_cells(self):
-        assert len(mesh['0']['cells']) == len(truth_mesh.cell_faces)
 
-    def test_read_cell_faces(self):
-        a_key = list(mesh['0']['cells'].keys())[0]
-        smithers_cell = mesh['0']['cells'][a_key]
+def test_read_points():
+    np.testing.assert_almost_equal(mesh["0"]["points"], truth_mesh.points)
 
-        np.testing.assert_almost_equal(
-            smithers_cell['faces'],
-            truth_mesh.cell_faces[a_key])
 
-    def test_read_cell_neighbors(self):
-        a_key = list(mesh['0']['cells'].keys())[-1]
-        smithers_cell = mesh['0']['cells'][a_key]
-        np.testing.assert_almost_equal(
-            smithers_cell['neighbours'],
-            truth_mesh.cell_neighbour[a_key])
+def test_read_faces():
+    np.testing.assert_almost_equal(mesh["0"]["faces"], truth_mesh.faces)
 
-    def test_read_cell_points(self):
-        a_key = list(mesh['0']['cells'].keys())[-1]
-        smithers_cell = mesh['0']['cells'][a_key]
 
-        faces_idxes = truth_mesh.cell_faces[a_key]
-        faces_points = np.concatenate([truth_mesh.faces[face_idx] for face_idx in faces_idxes])
-        faces_points = np.unique(faces_points)
+def test_read_cells():
+    assert len(mesh["0"]["cells"]) == len(truth_mesh.cell_faces)
 
-        np.testing.assert_almost_equal(
-            smithers_cell['points'],
-            faces_points
-        )
 
-    def test_boundary(self):
-        ofpp_obstacle = truth_mesh.boundary[b'obstacle']
-        smithers_obstacle = mesh['0']['boundary'][b'obstacle']
+def test_read_cell_faces():
+    a_key = list(mesh["0"]["cells"].keys())[0]
+    smithers_cell = mesh["0"]["cells"][a_key]
 
-        ofpp_obstacle_faces = truth_mesh.faces[
-            ofpp_obstacle.start:ofpp_obstacle.start + ofpp_obstacle.num]
+    np.testing.assert_almost_equal(
+        smithers_cell["faces"], truth_mesh.cell_faces[a_key]
+    )
 
-        np.testing.assert_almost_equal(mesh['0']['faces'][smithers_obstacle['faces']],
-            ofpp_obstacle_faces)
 
-        points_indexes = np.concatenate([face_points_idx for face_points_idx in ofpp_obstacle_faces])
-        points_indexes = np.unique(points_indexes)
-        all_points = truth_mesh.points[points_indexes]
+def test_read_cell_neighbors():
+    a_key = list(mesh["0"]["cells"].keys())[-1]
+    smithers_cell = mesh["0"]["cells"][a_key]
+    np.testing.assert_almost_equal(
+        smithers_cell["neighbours"], truth_mesh.cell_neighbour[a_key]
+    )
 
-        np.testing.assert_almost_equal(mesh['0']['points'][smithers_obstacle['points']], all_points)
 
-        assert smithers_obstacle['points'].ndim == 1
-        assert smithers_obstacle['faces'].ndim == 1
+def test_read_cell_points():
+    a_key = list(mesh["0"]["cells"].keys())[-1]
+    smithers_cell = mesh["0"]["cells"][a_key]
 
-    def test_read_fields_time_instants_all(self):
-        all_numeric_mesh = handler.read(openfoam_mesh_path, fields_time_instants='all_numeric')
-        assert set(all_numeric_mesh.keys()) == set(['0', '1088', '4196'])
+    faces_idxes = truth_mesh.cell_faces[a_key]
+    faces_points = np.concatenate(
+        [truth_mesh.faces[face_idx] for face_idx in faces_idxes]
+    )
+    faces_points = np.unique(faces_points)
 
-    def test_read_fields_time_instants_first(self):
-        assert set(mesh.keys()) == set(['0'])
+    np.testing.assert_almost_equal(smithers_cell["points"], faces_points)
 
-    def test_read_fields_time_instants_list(self):
-        handler = OpenFoamHandler()
-        time_list_mesh = handler.read(openfoam_mesh_path, fields_time_instants=['1088'])
-        assert set(time_list_mesh.keys()) == set(['1088'])
 
-    def test_read_fields_all(self):
-        for tdc in mesh.values():
-            assert set(tdc['fields'].keys()) == set(['U', 'p'])
+def test_boundary():
+    ofpp_obstacle = truth_mesh.boundary[b"obstacle"]
+    smithers_obstacle = mesh["0"]["boundary"][b"obstacle"]
 
-    def test_read_fields_list(self):
-        fields_list_mesh = handler.read(openfoam_mesh_path, field_names=['p'])
-        for tdc in fields_list_mesh.values():
-            assert set(tdc['fields'].keys()) == set(['p'])
+    ofpp_obstacle_faces = truth_mesh.faces[
+        ofpp_obstacle.start : ofpp_obstacle.start + ofpp_obstacle.num
+    ]
 
-    def test_no_time_instants(self):
-        # assert that this doesn't raise anything
-        handler.read(notime_openfoam_mesh_path)
+    np.testing.assert_almost_equal(
+        mesh["0"]["faces"][smithers_obstacle["faces"]["faces_indexes"]],
+        ofpp_obstacle_faces,
+    )
+
+    points_indexes = np.concatenate(
+        [face_points_idx for face_points_idx in ofpp_obstacle_faces]
+    )
+    points_indexes = np.unique(points_indexes)
+    all_points = truth_mesh.points[points_indexes]
+
+    np.testing.assert_almost_equal(
+        mesh["0"]["points"][smithers_obstacle["points"]], all_points
+    )
+
+    assert smithers_obstacle["points"].ndim == 1
+    assert (
+        isinstance(smithers_obstacle["faces"]["faces_indexes"], list)
+        or smithers_obstacle["faces"]["faces_indexes"].ndim == 1
+    )
+
+
+def test_read_fields_time_instants_all():
+    all_numeric_mesh = handler.read(
+        openfoam_mesh_path, time_instants="all_numeric"
+    )
+    assert set(all_numeric_mesh.keys()) == set(["0", "1088", "4196"])
+
+
+def test_read_fields_time_instants_first():
+    assert set(mesh.keys()) == set(["0"])
+
+
+def test_read_fields_time_instants_list():
+    handler = OpenFoamHandler()
+    time_list_mesh = handler.read(openfoam_mesh_path, time_instants=["1088"])
+    assert set(time_list_mesh.keys()) == set(["1088"])
+
+
+def test_read_fields_all():
+    for tdc in mesh.values():
+        assert set(tdc["fields"].keys()) == set(["U", "p"])
+
+
+def test_read_fields_list():
+    fields_list_mesh = handler.read(openfoam_mesh_path, field_names=["p"])
+    for tdc in fields_list_mesh.values():
+        assert set(tdc["fields"].keys()) == set(["p"])
+
+
+def test_no_time_instants():
+    # assert that this doesn't raise anything
+    handler.read(notime_openfoam_mesh_path)
+
+
+def test_area():
+    np.testing.assert_almost_equal(
+        mesh["0"]["boundary"][b"obstacle"]["faces"]["area"][100],
+        0.039269502373542965,
+        decimal=7,
+    )
+
+
+def test_normal():
+    pts = np.array(
+        [
+            [0.00670972, 0.0209654, 0.0999091],
+            [0.00753623, 0.0213154, 0.0995543],
+            [0.00765566, 0.0214058, 0.100427],
+            [0.007578, 0.0208206, 0.100599],
+        ]
+    )
+    vecs = pts[1 : len(pts)] - pts[0]
+
+    nrm = OpenFoamHandler._normal(pts)
+    dots = np.dot(vecs, nrm)
+
+    np.testing.assert_allclose(dots[dots != 0], 0, atol=1e-4)
