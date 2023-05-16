7199dbd531487ec35e843a8f5971b7f3061db4e3
Dario Coscia <93731561+dario-coscia@users.noreply.github.com>
2023-05-16 16:56:41 +0200

    Lid dataset (#43)
    * lid dataset
    
    ---------
    
    Co-authored-by: Dario Coscia <dariocoscia@cli-10-110-33-142.WIFIeduroamSTUD.units.it>

--- smithers/dataset/__init__.py
+++ smithers/dataset/__init__.py
@@ -4,3 +4,4 @@ from .datasets.termal import TermalDataset
 from .datasets.navier_stokes import NavierStokesDataset
 from .datasets.graetz import GraetzDataset
 from .datasets.unsteady_heat import UnsteadyHeatDataset
+from .datasets.lid_cavity import LidCavity
